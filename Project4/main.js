456230f7cbe8079b5b525bf217390c9731012865
Nguyễn Lê Thành Tâm <82741052+tamnguyen662001@users.noreply.github.com>
2021-05-14 15:36:22 +0700

    Finish Project 4

--- Project4/main.js
+++ Project4/main.js
@@ -14,7 +14,11 @@ $(document).ready(function () {
             }
         }
     })
-
+    //===============Preload=====================
+    $(window).on('load',function(){
+        $('.preload').fadeOut(2500);
+       
+    }) 
     // ==============navbar shrink================
 
     $(window).on('scroll', function () {
@@ -140,4 +144,153 @@ function realtimeClock() {
     h + " : "+  m+" : "+ s + " : " + amPm ;
 
     var l = setTimeout(realtimeClock,500);
+}
+
+ let pro = document.getElementById("progress");
+ let totalHeight = document.body.scrollHeight - window.innerHeight;
+ window.onscroll = function(){
+    let proHeight = (window.pageYOffset/totalHeight)*100;
+    pro.style.height= proHeight+'%';
+ }
+
+
+
+//  ================Toast menu==================
+function toast({ title = "", message = "", type = "info", duration = 5000 }) {
+    const main = document.getElementById("toast");
+    if (main) {
+      const toast = document.createElement("div");
+      // Auto remove toast
+      const autoRemoveId = setTimeout(function () {
+        main.removeChild(toast);
+      }, duration + 1000);
+      // Remove toast when clicked
+      toast.onclick = function (e) {
+        if (e.target.closest(".toast__close")) {
+          main.removeChild(toast);
+          clearTimeout(autoRemoveId);
+        }
+      };
+  
+      const icons = {
+        success: "fas fa-code",
+      };
+      const icon = icons[type];
+      const delay = (duration / 1000).toFixed(2);
+  
+      toast.classList.add("toast", `toast--${type}`);
+      toast.style.animation = `slideInLeft ease 2s, fadeOut linear 2s ${delay}s forwards`;
+  
+      toast.innerHTML = `
+                      <div class="toast__icon">
+                         <i class="${icon}"></i>
+                      </div>
+                      <div class="toast__body">
+                          <h3 class="toast__title">${title}</h3>
+                          <p class="toast__msg">${message}</p>
+                      </div>
+                      <div class="toast__close">
+                          <i class="fas fa-times"></i>
+                      </div>
+                  `;
+      main.appendChild(toast);
+    }
+  }
+  function showSuccessToast() {
+    toast({
+      title: "Welcom to Speedcubers",
+      message: "Get started !",
+      type: "success",
+      duration: 5000
+    });
+  }
+  function a(){
+      showSuccessToast();
+  }
+// =========================== Form modal================
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const form = document.getElementById('form-1');
+
+
+  form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
+
+function checkInputs() {
+    var check = true;
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+
+    
+
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+       
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+      
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+    
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+     
+	} else{
+		setSuccessFor(password2); 
+	}
+
+    
+    var arr = [usernameValue, emailValue,passwordValue, password2Value];
+    console.log(arr);
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index] == ""){
+            check = false;
+        }
+        else if(arr[arr.length] != arr[arr.length-1]){
+            check= false;
+        }  
+    }
+    if(check){
+        showLoginToast();
+    }
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-group error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-group success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
