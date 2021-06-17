37d8696c3251aed5a17ccfe89d8284f67a32d3a1
Nguyễn Lê Thành Tâm <82741052+tamnguyen662001@users.noreply.github.com>
2021-06-17 22:47:18 +0700

    add file thi thử

---
+++ Thi thử WEB/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Nếu có lỗi khi chưa kéo đến main menu mà nó đã thực hiện function này, mong thầy reload lại trang và kiểm tra giúp em ạ!!
+    // Scroll chuột đến main menu, vượt quá sẽ gắn thêm class fixed-top để hiển thị main menu
+    // pos = $('#main-menu').position();
+    // $(document).on('scroll', function () {
+    //     if ($(this).scrollTop() > parseInt(pos.top)) {
+    //         $('#main-menu').addClass('fixed-top');
+    //     } else {
+    //         $('#main-menu').removeClass('fixed-top');
+    //     }
+    // });
+    // Đóng vùng navbar collapse sau khi click luân phiên 2 button toggler
+    $('.float-left').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    })
+
+    $('.float-right').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    })
+});
